2871a2b2a5ed6b871deb7e4914b3d319c4b77c76
loxt <emannuelloxt@hotmail.com>
2020-11-22 14:32:24 -0400

    chore(WIP): stylize portfolio items

--- freelancer-portfolio/js/app.js
+++ freelancer-portfolio/js/app.js
@@ -10,6 +10,20 @@ async function loadPortfolio() {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      let html = '';
+
+      data.portfolio.forEach(
+        (item) =>
+          (html += `
+          <div class="item">
+            <img src="img/${item.id}.jpg" />
+            <div class="content">
+              <h3>${item.projectname}</h3>
+              <p>${item.desc}</p>
+            </div>
+          </div>
+        `)
+      );
+      document.querySelector('#portfolio').innerHTML = html;
     });
 }
